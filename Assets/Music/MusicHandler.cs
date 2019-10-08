1f41ceafcaad4175f914ef0822125293e568c90f
Sascha Lüscher <sascha.luescher@stud.kftg.ch>
2019-10-08 16:41:39 +0200

    Working Settings
    The Settings Options are working now.

---
+++ Assets/Music/MusicHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicHandler : MonoBehaviour
+{
+    //Music Sprites
+    public Sprite MusicOn, MusicStop;
+    Image MusicSR;
+
+    void Start()
+    {
+        //Set up the current Status
+        MusicSR = GameObject.Find("Sound").GetComponent<Image>();
+        ChangeSprite();
+    }
+
+    //Turn the Volume on/off and change sprite
+    public void ChangeVolume()
+    {
+        GlobalSettings.AudioOn = !GlobalSettings.AudioOn;
+        ChangeSprite();
+
+    }
+
+    //Change Sprite according to the current Audio Status
+    void ChangeSprite()
+    {
+        if (GlobalSettings.AudioOn)
+        {
+            MusicSR.sprite = MusicOn;
+        }
+        else
+        {
+            MusicSR.sprite = MusicStop;
+        }
+    }
+
+}
--- Assets/Options/OptionHandler.cs
+++ Assets/Options/OptionHandler.cs
@@ -2,33 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionHandler : MonoBehaviour
 {
-    //Music Stuff
-    public Sprite MusicOn, MusicStop;
-    SpriteRenderer MusicSR;
 
-    private void Start()
-    {
-    }
-
-    //Turn the Volume on/off and change sprite
-    public void ChangeVolume()
-    {
-        MusicSR = GameObject.Find("Sound").GetComponent<SpriteRenderer>();
-        GlobalSettings.AudioOn = !GlobalSettings.AudioOn;
-        if (GlobalSettings.AudioOn)
-        {
-            MusicSR.sprite = MusicOn;
-        }
-        else
-        {
-            MusicSR.sprite = MusicStop;
-        }
-
-    }
-
+   
     //Change between Menu and options
     public void BackToMenu()
     {
